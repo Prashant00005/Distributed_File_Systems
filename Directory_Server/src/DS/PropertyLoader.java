580a21670635612afbc15aeab4805e39a1416eab
Prashant Aggarwal <aggarwap@tcd.ie>
2017-12-14 19:24:25 +0000

    Updated PropertyLoader.java

--- Directory_Server/src/DS/PropertyLoader.java
+++ Directory_Server/src/DS/PropertyLoader.java
@@ -7,12 +7,7 @@ public  class PropertyLoader {
 
 	public static String Url_Server;
 	public static String UrlRequest;
-	public static String Url_DirectoryServer;
-	public static String Url_LockServer;
-	public static String Url_loginRequest;
-	public static String Url_FileRead;
-	public static String Url_GetLock;
-	public static String Url_FileInfo;
+	
 	
 
 	public static void loadProperties() {
@@ -25,17 +20,7 @@ public  class PropertyLoader {
 		
 			UrlRequest = config.getProperty("UrlRequest");
 					
-			Url_DirectoryServer = config.getProperty("Url_DirectoryServer");
 			
-			Url_LockServer = config.getProperty("Url_LockServer");
-			
-			Url_loginRequest = config.getProperty("Url_loginRequest");
-			
-			Url_FileRead = config.getProperty("Url_FileRead");
-			
-			Url_GetLock = config.getProperty("Url_GetLock");
-			
-			Url_FileInfo = config.getProperty("Url_FileInfo");
 			
 			
 		} catch (IOException e) {
